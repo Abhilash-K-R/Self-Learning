2e91a7fef983f7b20406bee9404f9a1c55d92cfc
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-05-23 13:46:42 +0530

    Add longestPalindrome function for palindrome detection
    Implement a solution to find the longest palindromic substring.

---
+++ Leetcode_Practice/Two_pointers/Long_palin.py
@@ -0,0 +1,28 @@
+# 5
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ""
+
+        def expand(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l + 1:r]
+
+        for i in range(len(s)):
+            # Odd length palindrome
+            p1 = expand(i, i)
+
+            # Even length palindrome
+            p2 = expand(i, i + 1)
+
+            if len(p1) > len(res):
+                res = p1
+            if len(p2) > len(res):
+                res = p2
+
+        return res
