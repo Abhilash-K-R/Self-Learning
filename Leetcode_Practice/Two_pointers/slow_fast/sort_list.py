57beb69be025e31f15fe3390d4e97f71381535a3
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-05-26 23:32:32 +0530

    Add sortList function to sort a linked list
    Implement merge sort for a linked list.

---
+++ Leetcode_Practice/Two_pointers/slow_fast/sort_list.py
@@ -0,0 +1,54 @@
+'''
+148. Sort List
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+'''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+
+            curr = curr.next
+
+        curr.next = l1 if l1 else l2
+
+        return dummy.next
+
+    def sortList(self, head):
+
+        if not head or not head.next:
+            return head
+
+        # find middle
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.merge(left, right)
