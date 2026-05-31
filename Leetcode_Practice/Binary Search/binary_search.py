6dc52fcc9b84bdbfc030d002c3428ccbc4efb419
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-05-31 08:32:36 +0530

    Add binary search function to find target in list
    Implement binary search algorithm to find target index.

---
+++ Leetcode_Practice/Binary Search/binary_search.py
@@ -0,0 +1,26 @@
+'''
+704. Binary Search
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+'''
+def search(nums,target):
+  low , high = 0 , len(nums)-1
+
+  while low <= high:
+    mid = low + (high - low)//2
+    if nums[mid] == target :
+      return mid
+  
+    if nums[low] <= target < nums[mid] :
+      high = mid -1
+    else :
+      low = mid + 1
+  return -1
