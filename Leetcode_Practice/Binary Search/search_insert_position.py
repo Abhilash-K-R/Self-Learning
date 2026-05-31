d0388e60a4f982b52f14079937c7e1aac2dad534
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-05-31 08:59:30 +0530

    Add searchInsert function for binary search
    Implement binary search to find the insert position for a target in a sorted array.

---
+++ Leetcode_Practice/Binary Search/search_insert_position.py
@@ -0,0 +1,27 @@
+'''
+35. Search Insert Position
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+'''
+def searchInsert(nums,target):
+  low , high = 0 ,len(nums)-1
+  while low <= high:
+    mid = low + (high-low)//2
+
+    if nums[mid] == target :
+      return mid
+    elif target < nums[mid] :
+      high = mid -1
+    else :
+      low = mid + 1
+  return low
