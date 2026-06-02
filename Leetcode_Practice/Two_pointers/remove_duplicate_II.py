5bc2e4a3203cb35e5286bb54226503f7d583a439
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-06-02 16:20:41 +0530

    Add solution for removing duplicates from sorted array II
    Implement the removeDuplicates method to remove duplicates from a sorted array, allowing at most two occurrences of each number.

---
+++ Leetcode_Practice/Two_pointers/remove_duplicate_II.py
@@ -0,0 +1,33 @@
+'''
+80. Remove Duplicates from Sorted Array II
+Example 1:
+
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+'''
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if len(nums) < 2: return len(nums)
+
+        slow = 2
+
+        for fast in range(2,len(nums)):
+            if nums[fast] != nums[slow-2]:
+                nums[slow] = nums[fast]
+                slow +=1
+
+        return slow
