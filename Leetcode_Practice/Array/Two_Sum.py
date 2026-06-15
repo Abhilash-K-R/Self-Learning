189f17405dbd81797885f7ad25d81373f62f4c93
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-06-15 18:45:46 +0530

    Implement brute force solution for two sum
    Added a brute force method for the two sum problem.

--- Leetcode_Practice/Array/Two_Sum.py
+++ Leetcode_Practice/Array/Two_Sum.py
@@ -42,3 +42,13 @@ class Solution(object):
                 left +=1
         
         return None
+
+# method 1 (Brute Force) 
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return[i, j]
+
+
