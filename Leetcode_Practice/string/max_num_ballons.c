ced83aa6c136062848a9eb86582da4224dddc510
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-06-22 10:58:15 +0530

    Add maxNumberOfBalloons function
    Implement function to calculate maximum number of 'balloon' words that can be formed from given text.

---
+++ Leetcode_Practice/string/max_num_ballons.c
@@ -0,0 +1,37 @@
+/*
+1189. Maximum Number of Balloons
+Example 1:
+
+
+
+Input: text = "nlaebolko"
+Output: 1
+Example 2:
+
+
+
+Input: text = "loonbalxballpoon"
+Output: 2
+*/
+
+int maxNumberOfBalloons(char* text) {
+    int freq[26] = {0};
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        freq[text[i] - 'a']++;
+    }
+
+    int b = freq['b' - 'a'];
+    int a = freq['a' - 'a'];
+    int l = freq['l' - 'a'] / 2;
+    int o = freq['o' - 'a'] / 2;
+    int n = freq['n' - 'a'];
+
+    int min = b;
+    if (a < min) min = a;
+    if (l < min) min = l;
+    if (o < min) min = o;
+    if (n < min) min = n;
+
+    return min;
+}
