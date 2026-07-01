2518afefbdadefb0af26be5994fea86485ab31f6
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-07-01 22:10:15 +0530

    Add coin_change.py for solving Coin Change problem
    Implement coin change solution using dynamic programming.

---
+++ Leetcode_Practice/Array/coin_change.py
@@ -0,0 +1,35 @@
+'''
+322. Coin Change
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+'''
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        if dp[amount] == amount + 1:
+            return -1
+        return dp[amount]
