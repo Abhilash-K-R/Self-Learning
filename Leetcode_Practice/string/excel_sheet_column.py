cfceb4eca907466c22112e38176e2d18e10f0d64
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-07-05 19:27:29 +0530

    Add function to convert column number to title
    Implement function to convert column number to Excel title.

---
+++ Leetcode_Practice/string/excel_sheet_column.py
@@ -0,0 +1,44 @@
+'''
+168. Excel Sheet Column Title
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+'''
+
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+        result = []
+
+        while columnNumber > 0:
+            columnNumber -= 1
+            result.append(chr(columnNumber % 26 + ord('A')))
+            columnNumber //= 26
+
+        return "".join(result[::-1])
