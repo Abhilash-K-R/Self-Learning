b0506505277755fd9652d884931058b203733f0f
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-07-16 22:50:55 +0530

    Add maximum product subarray solution
    Implement solution for finding maximum product subarray.

---
+++ Leetcode_Practice/Array/maximum_product_subarray.py
@@ -0,0 +1,40 @@
+'''
+152. Maximum Product Subarray
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Note that the product of an array with a single element is the value of that element.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray
+'''
+
+class Solution(object):
+    def maxProduct(self, nums):
+        max_prod = nums[0]
+        min_prod = nums[0]
+        ans = nums[0]
+
+        for num in nums[1:]:
+
+            if num < 0:
+                max_prod, min_prod = min_prod, max_prod
+
+            max_prod = max(num, max_prod * num)
+            min_prod = min(num, min_prod * num)
+
+            ans = max(ans, max_prod)
+
+        return ans
+      
