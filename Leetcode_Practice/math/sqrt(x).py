f0db195abcb0aede00288ef30b78481f25182ace
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-08-01 23:14:23 +0530

    Add mySqrt function for calculating square root
    Implement the mySqrt function to calculate the integer square root of a given number.

---
+++ Leetcode_Practice/math/sqrt(x).py
@@ -0,0 +1,22 @@
+'''
+69. Sqrt(x)
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+ '''
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        i = 0
+        while i * i <= x:
+            i += 1
+
+        return i - 1
