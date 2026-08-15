d876706f166d052ddb7b8f4c06fff30582c709c9
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-08-15 21:46:35 +0530

    Add toLowerCase method in To_Lower_case.py
    Implement the 'toLowerCase' method to convert a string to lowercase.

---
+++ Leetcode_Practice/string/To_Lower_case.py
@@ -0,0 +1,20 @@
+'''
+709. To Lower Case
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+ '''
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s = s.lower()
+        return s
