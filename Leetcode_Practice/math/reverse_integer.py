36e14e0342cde8d032ded3cb0e620ef7d4c4e9f7
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2026-09-09 20:58:58 +0530

    Refactor reverse integer function

--- Leetcode_Practice/math/reverse_integer.py
+++ Leetcode_Practice/math/reverse_integer.py
@@ -18,12 +18,8 @@ class Solution:
     def reverse(self, x: int) -> int:
         sign = -1 if x < 0 else 1  # assign sign
         x = abs(x) # convert positive
-
         rev = int(str(x)[::-1]) #reverse
-
         rev *=sign # handels sign - or +
-
         if rev < -2**31 or rev > 2**31 - 1:  # manage limits
             return 0
-
         return rev 
